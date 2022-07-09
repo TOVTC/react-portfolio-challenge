45261ff161bc838054bc10b85020fbbdedbf0d98
TOVTC <veronica.tc.to@outlook.com>
2022-07-08 20:56:49 -0400

    added highlighting to selected option, completed Nav

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, {useState} from 'react';
 import Navigation from './components/Navigation';
 
 function App() {
@@ -7,9 +8,17 @@ function App() {
         "Contact",
         "Resume"
     ]
+    const [selection, setSelection] = useState(options[0]);
     return (
         <div>
-            <Navigation options={options}></Navigation>
+            {/* use variables to update highlighted text in Nav + set the currently selected section */}
+            <Navigation
+                options={options}
+                selection={selection}
+                setSelection={setSelection}
+            >
+            </Navigation>
+            {/* here, take the selection and render each section using a switch statement? */}
         </div>
     );
 }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,6 +1,11 @@
 import React from 'react';
 
-function Navigation({options}) {
+function Navigation(props) {
+    const {
+        options = [],
+        selection,
+        setSelection
+    } = props;
     return (
         <header className='d-flex justify-content-between p-5'>
             <h1 className='d-inline-flex'>
@@ -9,8 +14,8 @@ function Navigation({options}) {
             <nav className='d-inline-flex'>
                 <ul className='flex-row list-inline'>
                     {options.map((option) => (
-                        <li className='mx-3 d-inline-flex' key={option}>
-                            <span onClick={() => {console.log(option)}}>
+                        <li className={`mx-3 d-inline-flex ${selection === option && 'navActive'}`} key={option}>
+                            <span onClick={() => setSelection(option)}>
                                 {option}
                             </span>
                         </li>
