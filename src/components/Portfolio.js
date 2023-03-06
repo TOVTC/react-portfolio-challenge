f6d4c4b7bb3513c270a00a6a9864758fc60d1683
TOVTC <veronica.tc.to@outlook.com>
2023-03-06 08:41:36 -0500

    updated portfolio and about me

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -6,15 +6,19 @@ import {BsGithub, BsLink} from "react-icons/bs";
 function Portfolio() {
     const [projects] = useState([
         // {name: '', deployed: true/false, focus: '', repo: '', link: '', img: ''},
-        {name: 'Locum', deployed: true, focus: 'React SPA', repo: 'https://github.com/SCScbc-Projects2022/project-three', link: 'https://locum-staffing.herokuapp.com/', img: 'locum'},
-        {name: 'Employee Tracker', deployed: false, focus: 'SQL Database', repo: 'https://github.com/TOVTC/employee-tracker-scscbc-challenge', img: 'employee-tracker'},
-        {name: 'Team Profile Generator', deployed: false, focus: 'Node.js', repo: 'https://github.com/TOVTC/team-profile-generator-scscbc-challenge', img: 'team-generator'},
+        {name: 'CatWorx', deployed: false, focus: 'C#, .NET Core', repo: 'https://github.com/TOVTC/csharp-.net-CatWorx', img: 'csharp-catworx'},
+        {name: 'Just Tech News', deployed: true, focus: 'Java Fullstack MVC', repo: 'https://github.com/TOVTC/tech-news-java-api', link: 'https://cc-java-api-777.herokuapp.com/', img: 'java-tech-news'},
+        {name: 'Java Employee Tracker', deployed: false, focus: 'Spring Boot API', repo: 'https://github.com/TOVTC/java-spring-employee-api', img: 'java-employee-api'},
+        {name: 'Hello, User!', deployed: false, focus: 'Java API', repo: 'https://github.com/TOVTC/spring-java-greeting-api', img: 'java-hello-user'},
+        {name: 'Locum', deployed: true, focus: 'React SPA', repo: 'https://github.com/TOVTC/Locum-project-three', link: 'https://locum-staffing.herokuapp.com/', img: 'locum'},
+        {name: 'Employee Tracker', deployed: false, focus: 'SQL Database', repo: 'https://github.com/TOVTC/employee-tracker-challenge', img: 'employee-tracker'},
+        {name: 'Team Profile Generator', deployed: false, focus: 'Node.js', repo: 'https://github.com/TOVTC/team-profile-generator-challenge', img: 'team-generator'},
         {name: 'Fermata', deployed: true, focus: 'Fullstack MVC', repo: 'https://github.com/TOVTC/Fermata-project-two', link: 'https://experience-fermata.herokuapp.com/home', img: 'fermata'},
-        {name: 'WeathCon', deployed: true, focus: 'Interactive Frontend', repo: 'https://github.com/TOVTC/WeathCon-project-one', link: 'https://scscbc-projects2022.github.io/WeathCon-project-one/', img: 'weathcon'},
-        {name: 'Weather Dashboard', deployed: true, focus: 'Server-Side APIs', repo: 'https://github.com/TOVTC/weather-dashboard-scscbc-challenge', link: 'https://tovtc.github.io/weather-dashboard-scscbc-challenge/', img: 'weather-dashboard'},
-        {name: 'Workday Scheduler', deployed: true, focus: 'Third-Party APIs', repo: 'https://github.com/TOVTC/scheduler-scscbc-challenge', link: 'https://tovtc.github.io/scheduler-scscbc-challenge/', img: 'scheduler'},
-        {name: 'Code Quiz', deployed: true, focus: 'Web APIs', repo: 'https://github.com/TOVTC/code-quiz-scscbc-challenge', link: 'https://tovtc.github.io/code-quiz-scscbc-challenge/', img: 'coding-quiz'},
-        {name: 'Personal Portfolio', deployed: true, focus: 'CSS Flexbox', repo: 'https://github.com/TOVTC/portfolio-scscbc-challenge', link: 'https://tovtc.github.io/portfolio-scscbc-challenge/', img: 'portfolio'},
+        {name: 'WeathCon', deployed: true, focus: 'Interactive Frontend', repo: 'https://github.com/TOVTC/WeathCon-project-one', link: 'https://projects2022.github.io/WeathCon-project-one/', img: 'weathcon'},
+        {name: 'Weather Dashboard', deployed: true, focus: 'Server-Side APIs', repo: 'https://github.com/TOVTC/weather-dashboard-challenge', link: 'https://tovtc.github.io/weather-dashboard-challenge/', img: 'weather-dashboard'},
+        {name: 'Workday Scheduler', deployed: true, focus: 'Third-Party APIs', repo: 'https://github.com/TOVTC/scheduler-challenge', link: 'https://tovtc.github.io/scheduler-challenge/', img: 'scheduler'},
+        {name: 'Code Quiz', deployed: true, focus: 'Web APIs', repo: 'https://github.com/TOVTC/code-quiz-challenge', link: 'https://tovtc.github.io/code-quiz-challenge/', img: 'coding-quiz'},
+        {name: 'Personal Portfolio', deployed: true, focus: 'CSS Flexbox', repo: 'https://github.com/TOVTC/portfolio-challenge', link: 'https://tovtc.github.io/portfolio-challenge/', img: 'portfolio'},
     ]);
     const deployedApps = projects.filter(project => project.deployed === true);
     const nondeployedApps = projects.filter(project => project.deployed === false);
